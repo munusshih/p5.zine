fa0e9f9b418498b338bb96d0aa059e88a6f55086
tuantinghuang <th2703@nyu.edu>
2024-12-17 21:27:55 -0500

    Updates from workshop session

---
+++ src/graphics.js
@@ -0,0 +1,40 @@
+p5.prototype.createZine = function (width, height, renderer) {
+    // Call createGraphics to get a new graphics object
+    let pg = this.createGraphics(width, height, renderer);
+
+    const originalText = pg.text;
+
+    // Add or override methods
+    pg.customBehavior = function () {
+        this.fill(255, 255, 255);
+        this.text("Zine Mode!", width / 2, height / 2);
+    };
+
+    pg.setup = function () {
+        this.textAlign(LEFT, CENTER)
+        //default change to center
+    }
+
+    pg.draw = function () {
+        this.elt.style.display = "block";
+    }
+
+    pg.text = function (str, x, y) {
+        originalText.call(this, str, x, y);
+
+        const canvasX = this.elt.offsetLeft;
+        const canvasY = this.elt.offsetTop;
+
+        // Create a paragraph element and position it
+        const p = createP(str);
+        p.position(canvasX + x, canvasY + y);
+        p.style('font-family', 'Arial')
+        p.style('font-size', '12px')
+        p.style('line-height', '0')
+        p.style('margin', '0px')
+        p.style('padding', '0px')
+        p.style("color", "blue"); // Optional: Style the text
+    }
+
+    return pg;
+};
--- src/zine.js
+++ src/zine.js
@@ -1,53 +1,73 @@
 import { jsPDF } from "jspdf";
 
+
+// cleanup
 let myCanvas;
+
+//zine page ratio
+const ratioWidth = 198;
+const ratioHeight = 306;
+
+//default zine page size
+const pWidth = 800;
+const pHeight = (pWidth / ratioWidth) * ratioHeight;
+
+// max size of a single page in px
 const maxSinglePageWidth = 400;
 const maxCanvasWidth = (maxSinglePageWidth * 2) / 0.7;
-const rWidth = 198;
-const rHeight = 306;
-const pWidth = 800;
-const pHeight = (pWidth / rWidth) * rHeight;
+
+cover = zinePage.createGraphics();
+cover.draw();
+
 let aWidth, aHeight, gap;
-let borderYes = true;
-p5.prototype.selfie = null;
+let isExporting = true;
+p5.prototype.SELFIE = null;
 p5.prototype.cover = null;
 p5.prototype.one = null;
 p5.prototype.two = null;
 p5.prototype.three = null;
 p5.prototype.back = null;
 let fR = 10;
-let pD = 1;
+let pD = 2;
 p5.prototype.all = [];
 
-p5.prototype.initZine = function() {
+//order: setup() (one-time)> initZine()(one-time) > setupZine()(looping) > draw()(looping) > drawZine (looping)
+p5.prototype.initZine = function () {
+  //create the global canvas
   myCanvas = this.createCanvas(windowWidth - 17, windowWidth * 3.3);
   myCanvas.parent("#myCanvas");
-  updateAdaptiveWidth();
+  calculatePageWidth();
 
+  //config
+  //define framerate and pixel density
   if (typeof window.zine === "object" && window.zine !== null) {
     fR = typeof zine.frameRate !== "undefined" ? zine.frameRate : fR;
     pD = typeof zine.pixelDensity !== "undefined" ? zine.pixelDensity : pD;
   }
 
+  changeTitle();
+
+  //create graphics for cover, back, and 3 spreads (double width)
+  //combine these two section into a function?
   this.cover = this.createGraphics(pWidth, pHeight);
   this.one = this.createGraphics(pWidth * 2, pHeight);
   this.two = this.createGraphics(pWidth * 2, pHeight);
   this.three = this.createGraphics(pWidth * 2, pHeight);
   this.back = this.createGraphics(pWidth, pHeight);
 
-  all.push(this.cover, this.one, this.two, this.three, this.back);
-
   this.cover.pageSize = "single";
-  this.one.pageSize = "full";
-  this.two.pageSize = "full";
-  this.three.pageSize = "full";
+  this.one.pageSize = "spread";
+  this.two.pageSize = "spread";
+  this.three.pageSize = "spread";
   this.back.pageSize = "single";
 
-  this.pixelDensity(1);
+  //an array of all the pages
+  all.push(this.cover, this.one, this.two, this.three, this.back);
+
+  //why set it here again? px and fr
+  this.pixelDensity(2);
   this.frameRate(fR);
   this.angleMode(DEGREES);
-  this.noStroke();
-  changeTitle();
 
   if (
     typeof window.zine !== "undefined" &&
@@ -59,30 +79,36 @@ p5.prototype.initZine = function() {
     // No action needed if cam is explicitly false
   } else {
     // Create capture if cam is not false
-    selfie = createCapture(VIDEO);
-    selfie.hide();
+    SELFIE = createCapture(VIDEO);
+    SELFIE.hide();
   }
 
   resizeZineCanvas();
 };
 p5.prototype.registerMethod("afterSetup", p5.prototype.initZine);
 
-p5.prototype.setupZine = function() {
-  if (borderYes) {
+p5.prototype.setupZine = function () {
+  if (!isExporting) {
+    //default pixel density for screen display
     this.pixelDensity(2);
+    /* graphic and canvas have separate pixel density
+      this is changing "graphic's" pixel density
+      graphic is a <canvas> that is not displayed*/
     changePixelDensity(2);
   } else {
+    //higher pixel density for exporting & printing
     this.pixelDensity(5);
     changePixelDensity(5);
   }
 
-  mousePosition();
+  //calculate mouse position on big canvas to match graphic's relative position
+  calculateMousePos();
 };
 
 p5.prototype.registerMethod("pre", p5.prototype.setupZine);
 
-p5.prototype.drawZine = function() {
-  if (borderYes) {
+p5.prototype.drawZine = function () {
+  if (!isExporting) {
     this.push();
 
     this.image(cover, width / 2 - aWidth / 2, gap, aWidth, aHeight);
@@ -170,19 +196,15 @@ p5.prototype.drawZine = function() {
 
 p5.prototype.registerMethod("post", p5.prototype.drawZine);
 
-p5.prototype.mousePosition = function() {
-  calMousePos([cover, one, two, three, back]);
-};
-
-p5.prototype.changePixelDensity = function(num) {
+p5.prototype.changePixelDensity = function (num) {
   all.map((el) => {
     el.pixelDensity(num);
   });
 };
 
-p5.prototype.calMousePos = function(arr) {
+p5.prototype.calculateMousePos = function () {
   const ratioer = pWidth / aWidth;
-  arr.map((el, i) => {
+  all.map((el, i) => {
     if (i === 0 || i === 4) {
       el.mouseX = constrain(
         (mouseX - (width / 2 - aWidth / 2)) * ratioer,
@@ -213,29 +235,29 @@ p5.prototype.calMousePos = function(arr) {
   });
 };
 
-p5.prototype.changeTitle = function() {
+p5.prototype.changeTitle = function () {
   // Check if 'zine' is defined and is an object
   if (typeof window.zine === "object" && window.zine !== null) {
     // Use optional chaining to safely access properties, with fallback values
     document.querySelector("#genTitle").innerHTML =
       window.zine?.title || "Default Title";
     document.querySelector("#author").innerHTML = window.zine?.author
-      ? `by <a href="${window.zine?.personalUrl || "#"}">${
-          window.zine?.author
-        }</a>`
+      ? `by <a href="${window.zine?.personalUrl || "#"}">${window.zine?.author
+      }</a>`
       : "by Unknown Author";
     document.querySelector("#des").innerHTML =
       window.zine?.description || "No description available.";
   }
 };
 
-p5.prototype.updateAdaptiveWidth = function() {
+p5.prototype.calculatePageWidth = function () {
   aWidth = Math.min((width / 2) * 0.7, maxSinglePageWidth);
-  aHeight = (aWidth / rWidth) * rHeight;
+  aHeight = (aWidth / ratioWidth) * ratioHeight;
   gap = aWidth / 4;
 };
 
-p5.prototype.resizeZineCanvas = function() {
+//can you talk about the numbers here?
+p5.prototype.resizeZineCanvas = function () {
   if (windowWidth >= maxCanvasWidth) {
     this.resizeCanvas(maxCanvasWidth, maxCanvasWidth * 3.3);
   } else {
@@ -243,7 +265,7 @@ p5.prototype.resizeZineCanvas = function() {
   }
 };
 
-p5.prototype.printSetting = function() {
+p5.prototype.printSetting = function () {
   const printRotate = angleMode() === "degrees" ? 90 : HALF_PI;
   changePixelDensity(pD);
 
@@ -279,12 +301,12 @@ p5.prototype.printSetting = function() {
   pop();
 };
 
-p5.prototype.windowResized = function() {
-  updateAdaptiveWidth();
+p5.prototype.windowResized = function () {
+  calculatePageWidth();
   resizeZineCanvas();
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("download-jpg")
     .addEventListener("click", downloadJPG);
@@ -294,17 +316,17 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function downloadJPG() {
-  borderYes = false;
+  isExporting = true;
   cover.save("0-genZ" + hour() + minute() + second() + ".jpg");
   one.save("1-genZ" + hour() + minute() + second() + ".jpg");
   two.save("2-genZ" + hour() + minute() + second() + ".jpg");
   three.save("3-genZ" + hour() + minute() + second() + ".jpg");
   back.save("4-genZ" + hour() + minute() + second() + ".jpg");
-  borderYes = true;
+  isExporting = false;
 }
 
 function downloadPDF() {
-  borderYes = false;
+  isExporting = true;
   printSetting();
   const imgData = canvas.toDataURL("image/jpeg", 1.0);
   const pdf = new jsPDF("p", "in", [8.5, 11]);
@@ -331,5 +353,5 @@ function downloadPDF() {
     pdf.save(fileName + ".pdf");
   }
 
-  borderYes = true;
+  isExporting = false;
 }
